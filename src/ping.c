c08fccc5d69a1db1f5996c6bc5e4700d8276dc4a
Sergey Zolotarev <sryze@yandex.com>
2018-12-29 00:16:58 +0600

    Fix errors in Cygwin build

--- src/ping.c
+++ src/ping.c
@@ -34,7 +34,7 @@
 
 #pragma pack(push, 1)
 
-#ifdef _WIN32
+#if defined _WIN32 || defined __CYGWIN__
     #ifdef _MSC_VER
         typedef unsigned __int8 uint8_t;
         typedef unsigned __int16 uint16_t;
